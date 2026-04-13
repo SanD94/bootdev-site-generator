84751295a3737d944592a36bcc30547193db4291
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-04-13 22:23:49 +0200

    add leaf html node with unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,3 +26,26 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props_to_html()})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self,
+                 tag: str | None,
+                 value: str | None,
+                 props: dict[str, str] | None = None
+                 ) -> None:
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError
+
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props_to_html()})"
+
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 
@@ -19,6 +19,20 @@ class TestHTMLNode(unittest.TestCase):
         with self.assertRaises(NotImplementedError):
             HTMLNode().to_html()
 
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_val_err(self):
+        with self.assertRaises(ValueError):
+            LeafNode(None, None).to_html()
+
+    def test_to_html_only_val(self):
+        leafNode = LeafNode(None, "hello world!")
+        htmlStr = "hello world!"
+        self.assertEqual(leafNode.to_html(), htmlStr)
+
+    def test_to_html_tag(self):
+        leafNode = LeafNode("a", "hello world!", {"href" : "boot.dev"})
+        htmlStr = "<a href=\"boot.dev\">hello world!</a>"
+        self.assertEqual(leafNode.to_html(), htmlStr)
 
 if __name__ == "__main__":
     unittest.main()
