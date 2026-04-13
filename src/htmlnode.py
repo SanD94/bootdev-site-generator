6832b293c28c63fda0b349167edc94e9e618969b
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-04-13 20:08:04 +0200

    add html node class

---
+++ src/htmlnode.py
@@ -0,0 +1,29 @@
+
+from typing import Self
+
+class HTMLNode:
+    def __init__(self, 
+                 tag: str | None,
+                 value: str | None,
+                 children: list[Self] | None,
+                 props: dict[str, str] | None
+                 ) -> None:
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self) -> str:
+        if self.props is None:
+            return ""
+
+        prop_str_list: list[str] = []
+        for prop, val in self.props.items():
+            prop_str_list.append(f" {prop}=\"{val}\"")
+        return "".join(prop_str_list)
+
+    def __repr__(self) -> str:
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props_to_html()})"
