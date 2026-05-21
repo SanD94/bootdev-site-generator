bd4a3f160aa2d64149fc8fb1b095337daabe2c49
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-05-21 23:22:30 +0200

    add block to block type method

--- src/markdown_utils.py
+++ src/markdown_utils.py
@@ -1,3 +1,16 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING   = "heading"
+    CODE      = "code"
+    QUOTE     = "quote"
+    ULIST     = "unordered_list"
+    OLIST     = "ordered_list"
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
 
@@ -5,3 +18,21 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = list(filter(lambda block: block != "", blocks))
 
     return blocks
+
+
+def block_to_block_type(md_block: str) -> BlockType:
+    lines = md_block.split("\n")
+
+    if re.match(r"^#{1,6} .+", md_block):
+        return BlockType.HEADING
+    if md_block.startswith("```\n") and md_block.endswith("```"):
+        return BlockType.CODE
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.ULIST
+    if all(line.startswith(f"{i}. ") for i, line in enumerate(lines, start=1)):
+        return BlockType.OLIST
+
+
+    return BlockType.PARAGRAPH
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_utils import markdown_to_blocks
+from markdown_utils import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -26,8 +26,84 @@ This is the same paragraph on a new line
             ],
         )
 
+class TestBlockToBlockType(unittest.TestCase):
+    def test_correct_heading(self):
+        block = "###### hello"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.HEADING
+        )
+
+    def test_wrong_heading(self):
+        block = "####### hello"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_correct_code(self):
+        block = """```\nconsole.log("Hello World!");\n```"""
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.CODE
+        )
+
+    def test_wrong_code(self):
+        block = """```\nconsole.log("Hello World!");\n``"""
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_correct_quote(self):
+        block = """> Nope\n> Hey"""
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.QUOTE
+        )
+
+    def test_wrong_quote(self):
+        block = """> Nope\n< Hey"""
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_correct_ulist(self):
+        block = """- nope\n- hey"""
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.ULIST
+        )
+
+    def test_wrong_ulist(self):
+        block = """- nope\n-- hey"""
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
 
 
+    def test_correct_olist(self):
+        block = """1. nope\n2. hey"""
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.OLIST
+        )
+
+    def test_wrong_olist(self):
+        block = """1. nope\n3. hey"""
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
 
 if __name__ == "__main__":
     unittest.main()
